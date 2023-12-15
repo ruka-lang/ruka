52e295ca8bb21948bae5b5b699f9992f3c70fe4c
dwclake <clake.dw@gmail.com>
2023-12-15 13:39:24 -0800

    initial commit

--- src/compiler/compiler.c
+++ src/compiler/compiler.c
@@ -4,6 +4,7 @@
 
 #include "./compiler.h"
 
+/* Compiles file at filename to out_filename with flags */
 int compile_file(const char* filename, const char* out_filename, int flags) {
     struct compile_process* process = compile_process_create(filename, out_filename, flags); 
     
