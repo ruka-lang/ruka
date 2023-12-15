a76025620de7687c2421227674ada78e00c3b091
dwclake <clake.dw@gmail.com>
2023-12-15 13:56:40 -0800

    initial commit

--- src/compiler/cprocess.c
+++ src/compiler/cprocess.c
@@ -4,7 +4,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
 #include "compiler.h"
 
 struct compile_process* compile_process_create(const char* filename, const char* filename_out, int flags) {
--- src/main.c
+++ src/main.c
@@ -3,10 +3,21 @@
  */
 
 #include <stdio.h>
-
 #include "compiler/compiler.h"
 
 int main(int argc, const char* argv[]) {
-    printf("Hello, world!\n");
+    int result = compile_file("examples/test.c", "examples/test", 0);
+    switch (result) {
+        case COMPILER_FILE_COMPILED_OK:
+            printf("Compiled successfully\n");
+            break;
+        case COMPILER_FAILED_WITH_ERRORS:
+            printf("Compilation failed with errors\n");
+            break;
+        default: 
+            printf("File compiled with unknown response\n");
+            break;
+    }
+
     return 0;
 }
