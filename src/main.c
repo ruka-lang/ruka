88902d934b5cab34f430a06b29622fcb04e78a3f
dwclake <clake.dw@gmail.com>
2023-12-17 23:06:11 -0800

    added basic cli to main

--- src/main.c
+++ src/main.c
@@ -3,21 +3,40 @@
  */
 
 #include <stdio.h>
+#include <sys/errno.h>
 #include "compiler/compiler.h"
 
-int main(int argc, const char** argv) {
-    int result = compile("test/files/test.rex", NULL, 0);
-    switch (result) {
-        case COMPILER_FILE_COMPILED_OK:
-            printf("Compiled successfully\n");
-            break;
-        case COMPILER_FAILED_WITH_ERRORS:
-            printf("Compilation failed with errors\n");
-            break;
-        default: 
-            printf("File compiled with unknown response\n");
-            break;
-    }
+const char* prompt = "Rex compiler\n"
+                     "version: 0.1.0\n"
+                     "\n"
+                     "Usage: rex-c <input> [outfile] [flags]\n";
 
+int main(int argc, const char** argv) {
+    /* Check if first argument was provided (required) */
+    if (argv[1]) {
+        const char* output = NULL;
+        
+        /* Check if second argument was provided (optional) */
+        if (argv[2]) {
+            output = argv[2];
+        }
+
+        int result = compile(argv[1], output, 0);
+        switch (result) {
+            case COMPILER_FILE_COMPILED_OK:
+                printf("Compiled successfully\n");
+                break;
+            case COMPILER_FAILED_WITH_ERRORS:
+                printf("Compilation failed with errors\n");
+                break;
+            default: 
+                printf("File compiled with unknown response\n");
+                break;
+        }
+    } else {
+        printf("%s", prompt);
+        return ENOENT;
+    }
+    
     return 0;
 }
