891f2a7a7457d4def03a91d24d7788f24dd67673
dwclake <clake.dw@gmail.com>
2023-12-23 13:48:49 -0800

    test progress

--- test/scanner/test_scanner.c
+++ test/scanner/test_scanner.c
@@ -37,18 +37,49 @@ compiler_t* test_compiler(char* source, char* filename) {
  *
  *
  */
-int token_compare(token_t* lhs, token_t rhs) {
+int token_compare(token_t* lhs, token_t* rhs) {
     switch (lhs->type) {
         case INTEGER:
             if (
-                lhs->type != rhs.type ||
-                lhs->flags != rhs.flags ||
-                lhs->data.llnum != rhs.data.llnum ||
-                lhs->between_brackets != rhs.between_brackets ||
-                lhs->whitespace != rhs.whitespace ||
-                lhs->pos.col != rhs.pos.col ||
-                lhs->pos.line != rhs.pos.line ||
-                strncmp(lhs->pos.filename, rhs.pos.filename, strlen(lhs->pos.filename)) != 0
+                lhs->type != rhs->type ||
+                lhs->flags != rhs->flags ||
+                lhs->data.llnum != rhs->data.llnum ||
+                lhs->between_brackets != rhs->between_brackets ||
+                lhs->whitespace != rhs->whitespace ||
+                lhs->pos.col != rhs->pos.col ||
+                lhs->pos.line != rhs->pos.line ||
+                strncmp(lhs->pos.filename, rhs->pos.filename, strlen(lhs->pos.filename)) != 0
+            ) {
+                return -1;
+            }
+
+            break;
+        case IDENTIFIER:
+        case KEYWORD:
+            if (
+                lhs->type != rhs->type ||
+                lhs->flags != rhs->flags ||
+                strncmp(lhs->data.sval, rhs->data.sval, strlen(lhs->data.sval)) != 0 ||
+                lhs->between_brackets != rhs->between_brackets ||
+                lhs->whitespace != rhs->whitespace ||
+                lhs->pos.col != rhs->pos.col ||
+                lhs->pos.line != rhs->pos.line ||
+                strncmp(lhs->pos.filename, rhs->pos.filename, strlen(lhs->pos.filename)) != 0
+            ) {
+                return -1;
+            }
+
+            break;
+        case SYMBOL:
+            if (
+                lhs->type != rhs->type ||
+                lhs->flags != rhs->flags ||
+                lhs->data.cval != rhs->data.cval ||
+                lhs->between_brackets != rhs->between_brackets ||
+                lhs->whitespace != rhs->whitespace ||
+                lhs->pos.col != rhs->pos.col ||
+                lhs->pos.line != rhs->pos.line ||
+                strncmp(lhs->pos.filename, rhs->pos.filename, strlen(lhs->pos.filename)) != 0
             ) {
                 return -1;
             }
@@ -80,19 +111,82 @@ int test_next_token() {
         goto test_exit;
     }
 
-    token_t* token = read_next_token(scanner);
-    result = token_compare(token, (token_t){
-        .type = INTEGER,
-        .pos.col = 1,
-        .pos.line = 1,
-        .pos.filename = filename,
-        .flags = 0,
-        .whitespace = false,
-        .between_brackets = NULL,
-        .data.llnum = 123
-    });
+    int expected_count = 6;
+    token_t expected_tokens[] = {
+        (token_t){
+            .type = INTEGER, 
+            .pos.col = 1, 
+            .pos.line = 1, 
+            .pos.filename = filename, 
+            .flags = 0, 
+            .whitespace = true, 
+            .between_brackets = NULL, 
+            .data.llnum = 123
+        },
+        (token_t){
+            .type = KEYWORD, 
+            .pos.col = 5, 
+            .pos.line = 1, 
+            .pos.filename = filename, 
+            .flags = 0, 
+            .whitespace = true, 
+            .between_brackets = NULL, 
+            .data.sval = "let"
+        },
+        (token_t){
+            .type = IDENTIFIER, 
+            .pos.col = 9, 
+            .pos.line = 1, 
+            .pos.filename = filename, 
+            .flags = 0, 
+            .whitespace = true, 
+            .between_brackets = NULL, 
+            .data.sval = "x"
+        },
+        (token_t){
+            .type = SYMBOL, 
+            .pos.col = 11, 
+            .pos.line = 1, 
+            .pos.filename = filename, 
+            .flags = 0, 
+            .whitespace = true, 
+            .between_brackets = NULL, 
+            .data.cval = '='
+        },
+        (token_t){
+            .type = INTEGER, 
+            .pos.col = 13, 
+            .pos.line = 1, 
+            .pos.filename = filename, 
+            .flags = 0, 
+            .whitespace = false, 
+            .between_brackets = NULL, 
+            .data.llnum = 12
+        },
+        (token_t){
+            .type = SYMBOL, 
+            .pos.col = 15, 
+            .pos.line = 1, 
+            .pos.filename = filename, 
+            .flags = 0, 
+            .whitespace = false, 
+            .between_brackets = NULL, 
+            .data.cval = ';'
+        }
+    };
 
-    printf("%s\n", token->pos.filename);
+    result = scan(scanner);
+    if (result != SCANNER_FILE_SCANNED_OK) goto test_exit;
+    if (scanner->tokens->elements != expected_count) {
+        result = 1;
+        goto test_exit;
+    }
+
+    for (int i = 0; i < scanner->tokens->elements; i++) {
+        result = token_compare(vector_at(scanner->tokens, i), &expected_tokens[i]);
+        if (result != 0) goto test_exit;
+    }
+
     test_exit:
         free(scanner);
         free(compiler);
