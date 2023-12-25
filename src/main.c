6436ac5079718c84f95b296989368b9f3c28945d
dwclake <clake.dw@gmail.com>
2023-12-25 11:04:07 -0800

    formatting

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/errno.h>
 #include "../includes/compiler.h"
 
@@ -41,5 +42,5 @@ int main(int argc, const char** argv) {
         printf("%s", prompt);
     }
     
-    return 0;
+    return EXIT_SUCCESS;
 }
