370949d8c53f36d6f3755cc7ee4b8ad67637a816
dwclake <clake.dw@gmail.com>
2024-01-13 23:02:24 -0800

    made error constructors return boxed errors

--- src/compiler/error.rs
+++ src/compiler/error.rs
@@ -29,12 +29,12 @@ impl CompileError {
     /// ```
     /// 
     /// ```
-    pub fn new(file: Rc<str>, msg: Box<str>, pos: Position) -> Self {
-        return Self{
+    pub fn new(file: Rc<str>, msg: Box<str>, pos: Position) -> Box<Self> {
+        return Box::new(Self{
             file,
             msg,
             pos
-        };
+        });
     }
 }
 
--- src/scanner/error.rs
+++ src/scanner/error.rs
@@ -29,12 +29,12 @@ impl ScannerError {
     /// ```
     ///
     /// ```
-    pub fn new(file: Rc<str>, msg: Box<str>, pos: Position) -> Self {
-        return Self{
+    pub fn new(file: Rc<str>, msg: Box<str>, pos: Position) -> Box<Self> {
+        return Box::new(Self{
             file, 
             msg, 
             pos
-        };
+        });
     }
 }
 
