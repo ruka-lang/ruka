b9204bd152d9ddd4c308a5893aef214e6d075038
dwclake <clake.dw@gmail.com>
2024-08-31 16:27:18 -0700

    update: reworked ast structure

--- src/parser/ast.zig
+++ src/parser/ast.zig
@@ -2,121 +2,64 @@
 // @created: 2024-04-13
 
 const rukac = @import("../root.zig");
-const Keyword = rukac.Scanner.Token.Keyword;
-const Kind = rukac.Scanner.Token.Kind;
+const Token = rukac.Scanner.Token;
 
 const std = @import("std");
 
-nodes: std.ArrayList(Node),
+nodes: std.ArrayList(Node2EB),
 allocator: std.mem.Allocator,
 
 const Ast = @This();
 
-pub const Expression = union(enum) {
-    Unit,
-    Tag: []const u8,
-    Integer: []const u8,
-    Float: []const u8,
-    Boolean: bool,
-    Block: std.ArrayList(Node),
-    If: struct {
-        condition: *Expression,
-        consequence: *Expression,
-        alternative: *Expression
+pub const Node2EB = struct {
+    tag: enum {
+        Unit,
+        Tag,
+        Integer,
+        Float,
+        Boolean,
+        String,
+        Block,
+        If,
+        Match,
+        FnDef,
+        Closure,
+        FnCall,
+        MethCall,
+        Prefix,
+        Infix,
+        Postfix,
+        Binding,
+        Return
     },
-    Match: struct {
-        value: *Expression,
-        cases: std.ArrayList(struct{
-            condition: *Expression,
-            consequence: *Expression
-        })
-    },
-    FnDef: struct {
-        name: []const u8,
-        params: std.ArrayList([]const u8),
-        block: *Expression,
-        arity: usize
-    },
-    Closure: struct {
-        name: []const u8,
-        params: std.ArrayList([]const u8),
-        block: *Expression,
-        context: std.ArrayList([]const u8),
-        arity: usize
-    },
-    FnCall: struct {
-        func: *Expression,
-        args: std.ArrayList(Expression)
-    },
-    Prefix: struct {
-        operator: Kind,
-        value: *Expression
-    },
-    Infix: struct {
-        operator: Kind,
-        lhs: *Expression,
-        rhs: *Expression
-    },
-    Postfix: struct {
-        operator: Kind,
-        value: *Expression
+    main_token: *Token,
+    data: struct {
+        lhs: *Node2EB,
+        rhs: *Node2EB
     },
 
-    pub fn deinit(self: Expression, allocator: std.mem.Allocator) void {
-        switch (self) {
-            .Block => |block| {
-                for (block.items) |s| s.deinit(allocator);
-                block.deinit();
-            },
-            .If => |i| {
-                i.condition.deinit(allocator);
-                i.consequence.deinit(allocator);
-                i.alternative.deinit(allocator);
-
-                allocator.destroy(i.condition);
-                allocator.destroy(i.consequence);
-                allocator.destroy(i.alternative);
-            },
-            .Match => |match| {
-                for (match.cases.items) |c| {
-                    c.consequence.deinit(allocator);
-                    c.condition.deinit(allocator);
-
-                    allocator.destroy(c.consequence);
-                    allocator.destroy(c.condition);
-                }
-            },
-            else => {}
-        }
+    pub fn init() Node2EB {
+        return Node2EB {
+            .tag = undefined,
+            .main_token = undefined,
+            .data = undefined
+        };
     }
-};
 
-const Node = union(enum) {
-    Binding: struct {
-        kind: Keyword,
-        name: []const u8,
-        value: Expression
-    },
-    Return: Expression,
-    Expression: Expression,
-
-    pub fn deinit(self: Node, allocator: std.mem.Allocator) void {
-        switch (self) {
-            .Binding => |binding| binding.value.deinit(allocator),
-            .Return => |ret| ret.deinit(allocator),
-            .Expression => |expression| expression.deinit(allocator)
-        }
+    pub fn deinit(self: Node2EB, allocator: std.mem.Allocator) void {
+        _ = self;
+        _ = allocator;
     }
 };
 
 pub fn init(allocator: std.mem.Allocator) Ast {
     return Ast {
-        .nodes = std.ArrayList(Node).init(allocator),
+        .nodes = std.ArrayList(Node2EB).init(allocator),
         .allocator = allocator
     };
 }
 
 pub fn deinit(self: Ast) void {
-    for (self.nodes.items) |node| node.deinit(self.allocator);
+    for (self.nodes.items) |*node| node.deinit(self.allocator);
     self.nodes.deinit();
 }
--- src/parser/parser.zig
+++ src/parser/parser.zig
@@ -12,21 +12,22 @@ pub const Ast = @import("ast.zig");
 
 test "idk" {
     var ast = Ast.init(std.testing.allocator);
-    try ast.nodes.append(.{ 
-        .Binding = .{
-            .kind = .Let,
-            .name = "x",
-            .value = .{ .Integer = "12" }
-        }
-    });
+    defer ast.deinit();
+    //try ast.nodes.append(.{
+    //    .Binding = .{
+    //        .kind = .Let,
+    //        .name = "x",
+    //        .value = .{ .Integer = "12" }
+    //    }
+    //});
 
-    var t = std.StringHashMap(Ast).init(std.testing.allocator);
-    defer t.deinit();
+    //var t = std.StringHashMap(Ast).init(std.testing.allocator);
+    //defer t.deinit();
 
-    try t.put("let x = 12", ast);
+    //try t.put("let x = 12", ast);
 
-    var iter = t.iterator();
-    while (iter.next()) |e| {
-        defer e.value_ptr.deinit();
-    }
+    //var iter = t.iterator();
+    //while (iter.next()) |e| {
+    //    defer e.value_ptr.deinit();
+    //}
 }
