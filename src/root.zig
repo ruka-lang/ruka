57dbe53c933acb371f09d7f08389477a2ba79ad8
dwclake <clake.dw@gmail.com>
2024-08-31 16:58:46 -0700

    refactor: detach cli specific code from library

--- src/root.zig
+++ src/root.zig
@@ -2,14 +2,12 @@
 // @created: 2024-03-04
 
 pub const util = @import("util.zig");
-pub const cli = @import("cli/cli.zig");
 pub const Compiler = @import("compiler/compiler.zig");
 pub const Scanner = @import("scanner/scanner.zig");
 pub const Parser = @import("parser/parser.zig");
 
-test "Test all rukac modules" {
+test "Test all rukac library modules" {
     _ = util;
-    _ = cli;
     _ = Compiler;
     _ = Scanner;
     _ = Parser;
