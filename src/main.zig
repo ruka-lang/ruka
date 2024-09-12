a161f79715a4b7bb5a96c8c2946ac183d65015f8
dwclake <clake.dw@gmail.com>
2024-09-12 06:48:34 -0700

    update: added logging utilities

--- src/main.zig
+++ src/main.zig
@@ -6,11 +6,23 @@ const cli = @import("cli/cli.zig");
 
 const std = @import("std");
 
+const log = std.log.scoped(.exe);
+
+pub const std_options: std.Options = .{
+    .log_level = switch (@import("builtin").mode) {
+        .Debug => .debug,
+        else => .info
+    },
+    .logFn = rukac.util.log
+};
+
 const GPA = std.heap.GeneralPurposeAllocator(.{});
 
 pub fn main() !void {
     var gpa: GPA = .init;
     defer _ = gpa.deinit();
+
+    try rukac.util.setup_logs(gpa.allocator());
 
     try cli.handle_args(gpa.allocator());
 }
